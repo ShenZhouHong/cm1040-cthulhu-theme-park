e8f96c156cfaee7c5b834b512898be5d981206e6
ShenZhouHong <shen@hong.io>
2020-01-01 22:28:54 -0500

    added boilerplate html/css/js files

---
+++ project-website/js/common.js
@@ -0,0 +1,9 @@
+/*
+  Container for DOM manipulations. Ensures that the DOM is only manipulated
+  when the page is completely loaded and ready.
+*/
+$(function () {
+    "use strict";
+
+    console.log( "Ready!" );
+});
